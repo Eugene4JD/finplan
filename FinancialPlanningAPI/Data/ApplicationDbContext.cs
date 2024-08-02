c2f5cbda02ac242e2f98a134618cd75cb7498862
Evgheni.Demcenco <Evgheni.demcenco@maib.md>
2024-08-02 14:09:37 +0300

    wipo

---
+++ FinancialPlanningAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using FinancialPlanningAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinancialPlanningAPI.Data;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+{
+    public DbSet<User> Users { get; set; }
+    public DbSet<Role> Roles { get; set; }
+    public DbSet<UserRole> UserRoles { get; set; }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<UserRole>()
+            .HasKey(ur => new { ur.UserId, ur.RoleId });
+
+        modelBuilder.Entity<UserRole>()
+            .HasOne(ur => ur.User)
+            .WithMany(u => u.UserRoles)
+            .HasForeignKey(ur => ur.UserId);
+
+        modelBuilder.Entity<UserRole>()
+            .HasOne(ur => ur.Role)
+            .WithMany(r => r.UserRoles)
+            .HasForeignKey(ur => ur.RoleId);
+    }
+}
+
