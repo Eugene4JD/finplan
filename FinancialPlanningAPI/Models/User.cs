48e6660e1fae284db49b1f38855193d0babd6268
Evgheni.Demcenco <Evgheni.demcenco@maib.md>
2024-08-02 14:09:37 +0300

    wipo

---
+++ FinancialPlanningAPI/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace FinancialPlanningAPI.Models;
+
+public class User
+{
+    public int Id { get; set; }
+    public string Username { get; set; }
+    public string PasswordHash { get; set; }
+    public ICollection<UserRole> UserRoles { get; set; }
+}
